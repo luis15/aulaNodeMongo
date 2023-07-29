ba93fc93410a8e3b63ef3a458b24ce245c92265b
Luis Antonio Coêlho <coelho.luisantonio@hotmail.com>
2023-07-29 13:12:08 -0300

    Update index.js to 80 port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const AlunoController = require("./controllers/aluno");
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
